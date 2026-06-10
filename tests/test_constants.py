0bf5fc92a789193d2dd79643273d7c6f2a6a517c
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2026-06-10 21:01:50 +0200

    earth_radius is deprecated

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -17,7 +17,6 @@ def _print_constants():
     check(gamma_p)
     check(mu_Xe129)
     check(gamma_Xe129)
-    check(earth_radius)
     a = u.Quantity(3.1, "g / cm**3 ")
     check(a.si)
     check(a.to("kg/m**3"))
