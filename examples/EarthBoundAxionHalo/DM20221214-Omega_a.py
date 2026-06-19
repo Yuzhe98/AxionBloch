2b7ad0a3cc4e489a618f36911a9007688470ce68
Yuzhe Zhang <yuhzhang@uni-mainz.de>
2026-06-19 18:31:28 +0200

    calculation of 2022 DM measurement

---
+++ examples/EarthBoundAxionHalo/DM20221214-Omega_a.py
@@ -0,0 +1,129 @@
+# plot the daily modulation of the axion gradient at various stations on Earth, for an Earth-bound axion halo with a given Compton frequency and axion-nucleon coupling. The modulation is due to the rotation of the Earth, which changes the direction of the station relative to the axion halo. The code solves the time-independent Schrödinger equation for the axion halo and then computes the gradient at each station as a function of time over one day. Finally, it plots the results.
+
+from axionbloch.dependency import *
+from axionbloch.EarthBoundAxionHalo import EarthBoundAxionHalo
+from axionbloch.Station import (Baltimore, BuenosAires, CapeTown, Geneva,
+                                Mainz, Mumbai, Sanya, Sydney, Tokyo)
+
+stations = [
+    Mainz,
+    Geneva,
+    Baltimore,
+    Tokyo,
+    Mumbai,
+    Sanya,
+    Sydney,
+    CapeTown,
+    BuenosAires,
+]
+# stations = [
+#     CapeTown,
+#     Mumbai,
+# ]
+# station = Baltimore
+rhoE_DM = 0.3 * unit.GeV / unit.cm**3
+
+# states_to_check = ["1s", "2s", "2p", "3s", "3p", "3d", "4s", "4p", "4d"]
+states_to_check = ["2p"]
+measurement_times = []
+halo = EarthBoundAxionHalo(
+    nu_a=1.348 * unit.MHz,  # axion Compton frequency in Hz
+    N=int(2**12),  # number of grid points
+    extent=128.0 * unit.R_earth,  # spatial extent of the grid in units of earth radius
+    g_aNN=1e-9 * unit.GeV**-1,  # axion-nucleon coupling
+    verbose=True,
+)
+
+halo.showValueAndUnits()
+
+
+halo.solve_TISE_3D(
+    l_vals=[1],  # angular momentum quantum number
+    max_n_r=64,  # maximum principal quantum number to plot
+    verbose=False,
+)
+
+# ── gradient at Mainz over 2022-12-14 ────────────────────────────────────────
+# TODO make this a function in GravHalo class.
+# from astropy.time import Time
+# import matplotlib.pyplot as plt
+# from matplotlib import gridspec
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+t0 = Time(datetime(2022, 12, 14, 13, 1, tzinfo=ZoneInfo("Europe/Berlin")))
+t_hours = np.linspace(0, 8, 8) * unit.hour
+meas_starts = t0 + t_hours  # every 1 hour
+
+for meas_start in meas_starts:
+    rms = halo.findrmsOmega_aOverTime(
+        stateNames=states_to_check,
+        station=Mainz,
+        meas_times=meas_start + np.linspace(0, 60, 12) * unit.minute,
+        truncRadius=3 * unit.R_earth,
+        # g_aNN=,
+        verbose=False,
+    )
+    print("meas_start =", meas_start)
+    print("rms =", rms)
+    print()
+
+
+# fig = plt.figure(figsize=(8.5 / 2.54, 8.5 / 2.54), dpi=300)
+# gs = gridspec.GridSpec(3, 1)
+# fig.subplots_adjust(left=0.22, bottom=0.12, right=0.67, top=0.93, hspace=0.12)
+
+# ax_r = fig.add_subplot(gs[0])
+# ax_theta = fig.add_subplot(gs[1], sharex=ax_r, sharey=ax_r)
+# ax_phi = fig.add_subplot(gs[2], sharex=ax_r, sharey=ax_r)
+# # check(result["grad_r"][0:10])
+# ax_r.plot(t_hours, result["Omega_a_r"], color="tab:blue")
+# # ax_r.plot(t_hours, result["grad_r"].imag, label="Im", color="tab:orange", ls="--")
+# ax_theta.plot(t_hours, result["Omega_a_theta"], color="tab:green")
+# # ax_theta.plot(t_hours, result["grad_theta"].imag, color="tab:orange", ls="--")
+# ax_phi.plot(t_hours, result["Omega_a_phi"], color="tab:brown")
+# # ax_phi.plot(t_hours, result["grad_phi"].imag, color="tab:orange", ls="--")
+
+# ax_phi.set_xlabel("time (hour)")
+
+
+# def _ylabel(label, q):
+#     u_str = q.unit.to_string("latex_inline")[1:-1]
+#     return f"${label}$\n$\\left({u_str}\\right)$"
+
+
+# ax_r.set_ylabel(
+#     _ylabel("\\Omega_a^r", result["Omega_a_r"]),
+#     rotation=0,
+#     loc="center",
+#     labelpad=22,
+# )
+# ax_theta.set_ylabel(
+#     _ylabel("\\Omega_a^\\theta", result["Omega_a_theta"]),
+#     rotation=0,
+#     loc="center",
+#     labelpad=22,
+# )
+# ax_phi.set_ylabel(
+#     _ylabel("\\Omega_a^\\phi", result["Omega_a_phi"]),
+#     rotation=0,
+#     loc="center",
+#     labelpad=22,
+# )
+
+# for ax in [ax_r, ax_theta]:
+#     plt.setp(ax.get_xticklabels(), visible=False)
+
+# # ax_r.legend(loc="upper left", bbox_to_anchor=(1.0, 1.0), fontsize=7)
+# fig.suptitle(
+#     f"Mainz  ·  {states_to_check[0]}  ·  $\\nu_a={halo.nu_a.to_value(unit.MHz):.3f}$ MHz\nstart from {t0}"
+# )
+# ylim_abs = 0
+# for ax in [ax_r, ax_phi, ax_theta]:
+#     _ylim_upper, _ylim_bottom = ax.get_ylim()
+#     ylim_abs = np.amax(np.abs([ylim_abs, (_ylim_upper), (_ylim_bottom)]))
+
+# ax_r.set_ylim(-1 * ylim_abs, ylim_abs)
+# plt.tight_layout()
+# plt.show()
